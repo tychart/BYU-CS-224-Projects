f87f33c02381ebf75a687e5fe2b6bb186dc74a66
tychart <tyler@thechartrands.com>
2024-01-25 23:58:29 -0700

    Updates

---
+++ myxxd/comprun.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [-b] <c_program_file>"
+    exit 1
+fi
+
+build_only=false
+
+# Check if the first argument is a flag
+if [ "$1" == "-b" ]; then
+    build_only=true
+    shift  # Remove the flag from the argument list
+fi
+
+c_file="$1"
+executable="${c_file%.c}"
+
+# Compile the C program
+gcc "$c_file" -o "$executable"
+
+# Check if compilation was successful
+if [ $? -eq 0 ]; then
+    echo "Compilation successful."
+
+    if [ "$build_only" = false ]; then
+        echo "Running the program..."
+        echo "----------------------------------------------"
+        
+        # Run the compiled program
+        "./$executable"
+
+        echo "----------------------------------------------"
+    else
+        echo "Build only. Execution skipped."
+    fi
+else
+    echo "Compilation failed."
+fi
